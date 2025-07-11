0498298cae865e02aca59bb99bf73c40f1ee0a67
Felipe Mora <80855125+FelipeMoraR@users.noreply.github.com>
2025-07-10 22:39:54 -0400

    solution with hash

--- 6. Two Sum/index.ts
+++ 6. Two Sum/index.ts
@@ -11,3 +11,22 @@ function twoSum(nums: number[], target: number): number[] {
     
     return solution;
 };
+
+function twoSum2(nums: number[], target: number): number[] {
+    const memory = new Map(); // Here we're going to save the value as a key and the index as a value
+    const solution = []
+    for (let i: number = 0; i < nums.length; i++) {
+        const numToFind = target - nums[i];
+        const numExist = memory.get(numToFind);
+        console.log(numExist);
+        if (numExist === undefined) {
+            memory.set(nums[i], i);
+            continue;
+        };
+        solution.push(numExist);
+        solution.push(i);
+        break
+    }
+
+    return solution;
+}; 
