3f270a6c185044e280d52dd05675a8838a96c003
Felipe Mora <80855125+FelipeMoraR@users.noreply.github.com>
2025-07-21 22:10:32 -0400

    [ADD] TopKFrequent O(n)

--- topKfrequent/index.ts
+++ topKfrequent/index.ts
@@ -17,3 +17,32 @@ function topKFrequent(nums: number[], k: number): number[] {
 
 const nums = [1, 2];
 const k = 2;
+
+// Other way to doit with O(n)
+function topKFrequent2(nums: number[], k: number): number[] {
+    const memo = new Map();
+    const numsOrdered = new Array(nums.length + 1).fill([]); // All position share the same pointer of an array
+    const numsToReturn: number[] = [];
+
+    for (let i: number = 0; i < nums.length; i++) {
+        if (memo.has(nums[i])) memo.set(nums[i], 1 + memo.get(nums[i]));
+        else memo.set(nums[i], 1);
+    }
+
+    for (let [k, v] of memo) {
+        const targetArray = [...numsOrdered[v]]; 
+        targetArray.push(k);
+        numsOrdered[v] = targetArray;
+    }
+
+    for (let j: number = numsOrdered.length - 1; j > 0; j--) {
+        if (numsOrdered[j].length === 0) continue;
+        
+        for (let i of numsOrdered[j]) {
+            if (numsToReturn.length === k) return numsToReturn
+            numsToReturn.push(i);
+        }
+    }
+
+    return numsToReturn
+};
